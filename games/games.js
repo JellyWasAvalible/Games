6dc7d76e21de6fe9c2901af634f4700d0cba0c69
Aerell <49218878+Minesraft2@users.noreply.github.com>
2022-09-16 09:30:14 -0500

    Update games.js

--- games/games.js
+++ games/games.js
@@ -1,7 +1,7 @@
-document.getElementById('snake').onclick = () => location.assign('games/snake');
-document.getElementById('ttt').onclick = () => location.assign('games/tictactoe');
-document.getElementById('tetris').onclick = () => location.assign('games/tetris');
-document.getElementById('2048').onclick = () => location.assign('games/2048');
-document.getElementById('pacman').onclick = () => location.assign('games/pacman');
-document.getElementById('test').onclick = () => location.assign('games/test');
-document.getElementById('exit').onclick = () => location.assign('/')
+document.getElementById('snake').onclick = () => location.assign('snake');
+document.getElementById('ttt').onclick = () => location.assign('tictactoe');
+document.getElementById('tetris').onclick = () => location.assign('tetris');
+document.getElementById('2048').onclick = () => location.assign('2048');
+document.getElementById('pacman').onclick = () => location.assign('pacman');
+document.getElementById('test').onclick = () => location.assign('test');
+document.getElementById('exit').onclick = () => location.assign('/Games')

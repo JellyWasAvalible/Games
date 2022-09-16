1d7a373199562ed9497fe7c080d77f136249594d
Aerell <49218878+Minesraft2@users.noreply.github.com>
2022-09-16 09:22:18 -0500

    Create games.js

---
+++ games/games.js
@@ -0,0 +1,7 @@
+document.getElementById('snake').onclick = () => location.assign('games/snake');
+document.getElementById('ttt').onclick = () => location.assign('games/tictactoe');
+document.getElementById('tetris').onclick = () => location.assign('games/tetris');
+document.getElementById('2048').onclick = () => location.assign('games/2048');
+document.getElementById('pacman').onclick = () => location.assign('games/pacman');
+document.getElementById('test').onclick = () => location.assign('games/test');
+document.getElementById('exit').onclick = () => location.assign('/')
